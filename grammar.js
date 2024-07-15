af1d162f7768f61dc8d5aab66167511293f78e36
Jordan Ellis Coppard <jc+git@wz.ht>
2024-07-16 00:43:28 +0800

    add basic function and attribute parsing

--- grammar.js
+++ grammar.js
@@ -1,3 +1,7 @@
+const REG_ALPHABETIC = /[a-zA-Z]/
+const REG_NUMERIC = /[0-9]/
+const REG_ASCII_PUNCTUATION = /[!"#$%&'()*+,\-./:;<=>?@\[\\\]^_`\{|\}~]/
+
 // Noir no longer allows arbitrarily-sized integers. Also, `U128` is a struct not a numeric type.
 const NUMERIC_TYPES = [
     // Signed.
@@ -12,6 +16,32 @@ const NUMERIC_TYPES = [
     'i64'
 ]
 
+// TODO: Attributes have some further captures in the Noir lexer, e.g. `foreign` captures a name afterwards. So do that also (and for the secondary attributes).
+// Functions can only have one primary attribute.
+const PRIMARY_ATTRIBUTES = [
+    'foreign',
+    'builtin',
+    'oracle',
+    'test',
+    'recursive',
+    'fold',
+    'no_predicates',
+    'test',
+    'field',
+]
+
+// Functions can have any number of secondary attributes.
+const SECONDARY_ATTRIBUTES = [
+    'deprecated',
+    'contract_library_method',
+    'abi',
+    'export',
+]
+
+// TODO: Code whitespace is \t \n \r and literal space.
+// TODO: Whitespace attributes like #[bing bong] are valid.. cancer. What does the canonical noir compiler think the attribute is called? `bing bong`?
+// Keyword::Pub
+
 module.exports = grammar({
   name: 'noir',
 
@@ -19,13 +49,16 @@ module.exports = grammar({
         source_file: $ => repeat($._definition),
 
         _definition: $ => choice(
-            $.function_definition
+            $.function_definition,
+            $.attribute,
         ),
 
         // Function definition canonical: https://github.com/noir-lang/noir/blob/d4e03d0/compiler/noirc_frontend/src/parser/parser/function.rs#L19-L52
         // TODO: Complete coverage.
         function_definition: $ => seq(
-            // TODO: Attributes; modifiers.
+            // TODO: Attributes.
+            optional($.function_modifiers),
+            optional($.visibility_modifier),
             'fn',
             $.identifier,
             // TODO: Generics.
@@ -33,6 +66,16 @@ module.exports = grammar({
             // TODO: Function return type.
             $.block
         ),
+
+        // TODO: Logic for `pub crate`.
+        visibility_modifier: $ => choice(
+            'pub'
+        ),
+
+        // TODO: Comptime.
+        function_modifiers: $ => repeat1(choice(
+            'unconstrained'
+        )),
 
         // Hi, there hi asd
         parameter_list: $ => seq(
@@ -46,6 +89,21 @@ module.exports = grammar({
             '{',
             // repeat($._statement),
             '}'
+        ),
+
+        attribute: $ => seq(
+            '#',
+            '[',
+            // TODO: It's actually a lot more lenient, replace with other regex later.
+            // TODO: Ask upstream if they intend for satanic attribute definitions.
+            // TODO: Splits on ( and ) and that being sub-tokens.
+            repeat1(choice(
+                ' ',
+                REG_ALPHABETIC,
+                REG_NUMERIC,
+                REG_ASCII_PUNCTUATION,
+            )),
+            ']',
         ),
 
         // _statement: $ => choice(
