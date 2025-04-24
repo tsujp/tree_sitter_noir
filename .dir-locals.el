187c36e6f8fb5f6aedae9d51a37cc04fc8296900
Jordan Ellis Coppard <jc+git@wz.ht>
2025-04-24 21:30:28 +0900

    change file-local variables application to avoid prompting
    - Constantly being asked if I want to apply 'risky' variables is the
      most annoying shit of all time. It appears the only way STILL to
      get rid of such a prompt is either to GLOBALLY allow 'risky'
      variables (incredibly stupid) or.. nothing. I thought perhaps trusted-content
      or changing enable-local-variables would work but the former in init.el
      appears to do nothing and the latter in .dir-locals.el so it applies
      only to our project does nothing since .dir-locals.el applies after a
      file is loaded, modes applied, and any file-local variables set. So,
      instead remove them and apply them as part of .dir-locals.el to shut
      these prompts the fuck up.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,17 @@
-((nil . ((project-vc-name . "tree-sitter noir"))))
+((nil . ((project-vc-name . "tree_sitter_noir")))
+ ;; XXX: Not using require because then a provide in ox-ngd and ox-tst would be required, and we'd have to modify the load-path here too. If the day comes ox-ngd and ox-tst won't change a lot we can swap to a require but for now reloading them every time is best.
+ (org-mode . ((eval . (progn
+                        (require 'org-inlinetask)
+                        (load (expand-file-name "ox-ngd.el") nil t t)
+                        (load (expand-file-name "ox-tst.el") nil t t)))
+              (eval . (progn
+                        ;; TODO: Probably just use file-truename providing buffer-name instead of the expand-file-name etc.
+                        (let ((bn (file-relative-name buffer-file-truename)))
+                          (cond ((string= bn "noir_grammar.org")
+                                 (setq-local org-todo-keyword-faces '(("TOIMPL" . org-warning)
+                                                                     ("BLOCK" . org-verbatim)))
+                                 (setq-local org-babel-header-args:js '((treesit . :any))))
+                                ((string= bn "test.org")
+                                 (add-hook 'after-save-hook #'tst-export-current 0 t)))))))))
+
+;; XXX: Had to do cond hack because .dir-locals.el applies after a file is loaded and its local variables read meaning we cannot use enable-local-variables :all specifically for that buffer-local context _before_ the file-local variables are read. Maybe ask about this in the emacs-devel ml because this means that STILl you can only globally trust settings which is fucking insane. Also the new trusted-content pointed at the file does nothing either. It should not be this hard to say "hey, for this file fuck off and let me set 'risky' variables without me having to GLOBALLY trust them".
